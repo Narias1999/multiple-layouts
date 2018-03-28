ba735f02671c92d87e88a68465400a2245fdb87e
Nicolás Arias <nmarias38@misena.edu.co>
2018-03-28 11:03:13 -0500

    creando layouts

---
+++ src/components/layouts/Login.vue
@@ -0,0 +1,14 @@
+<template>
+    <v-app>
+        <v-toolbar>
+            <v-toolbar-title>Login</v-toolbar-title>
+            <v-spacer></v-spacer>
+            <v-toolbar-items class="hidden-sm-and-down">
+                <v-btn flat>Registrarse</v-btn>
+            </v-toolbar-items>
+        </v-toolbar>
+        <v-content>
+            <router-view/>
+        </v-content>        
+    </v-app>
+</template>
